b7734b9030609e441424ac2dc189113203be7429
agrero <thegrero@gmail.com>
2024-08-29 11:35:22 -0700

    changed directory name to avoid conflicts

---
+++ discord_app/GuildManager.py
@@ -0,0 +1,20 @@
+from discord_app.G
+
+class GuildManager:
+
+    def __init__(self) -> None:
+        pass
+    
+
+    def create_guild(self):
+
+        pass
+
+#await create_text_channel
+
+# need to be able to make text channel
+
+# change permissions to make it private (Different class)
+# - per user based on discord_id
+
+
--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 if __name__ == '__main__':
 
     import os
-    import discord
+    import discord_app
 
-    from discord.DiscordClient import CustomClient
+    from discord_app.DiscordClient import CustomClient
 
     # ah what another good looking candidate for some data validation 
     # using pydantic models
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     GUILD = os.getenv('DISCORD_ROOT')
     API_URL = os.getenv('URL_ROOT')
 
-    intents = discord.Intents.default()
+    intents = discord_app.Intents.default()
     intents.message_content = True
 
     client = CustomClient(intents=intents)
